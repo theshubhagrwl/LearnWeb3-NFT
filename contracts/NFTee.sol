29482d812c83c43c53a568d876cd5e46362a591b
Shubh Agrawal <shubhagrawal1280@gmail.com>
2022-06-26 11:03:18 +0530

    mint 5 NFT

--- contracts/NFTee.sol
+++ contracts/NFTee.sol
@@ -1,6 +1,16 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract NFTee {
-    //
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+//contract NFTee is an 'ERC721' contract
+contract NFTee is ERC721 {
+    constructor() ERC721("LearnWeb3's NFT", "LEARN-NFT") {
+        //mint 5 NFT
+        _mint(msg.sender, 1);
+        _mint(msg.sender, 2);
+        _mint(msg.sender, 3);
+        _mint(msg.sender, 4);
+        _mint(msg.sender, 5);
+    }
 }
